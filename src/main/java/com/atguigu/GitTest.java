a36bdb512493eef81d6a9396efabb7429ce59f3d
zengdawei <1737792495@qq.com>
2022-06-29 16:19:20 +0800

    first commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -4,6 +4,9 @@ public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello test");
+        System.out.println("hello git");
+
     }
 
 }
